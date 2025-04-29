c02bf2518337612da283eef23f11df04c820e28f
samuel <saylsev699@g.educaand.es>
2025-04-29 09:42:41 +0200

    Método para obtener las fotos de los genios finalizado

--- src/components/gallery/gallery.component.ts
+++ src/components/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
-import { forkJoin } from 'rxjs';
+import { catchError, forkJoin } from 'rxjs';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
@@ -32,6 +33,7 @@ export class GalleryComponent  implements OnInit {
   }
 
   getListNames(arrayNamesGenious: string[]) {
+    // El método forkJoin nos permite suscribirnos a varios Observables a la vez y terminará cuando los haya procesado todos
     forkJoin({
       math: this.apiService.getNamesMathGenious(),
       physic: this.apiService.getNamesPhysicGenious(),
@@ -39,6 +41,7 @@ export class GalleryComponent  implements OnInit {
     }).subscribe({
       next: ({ math, physic, informatic }) => {
         // Tengo que modificar los nombres obtenidos , ya que devuelve Categoría:Nombre del genio
+        // (...) Nos permite descomponer un array en objetos individuales 
         arrayNamesGenious.push(...math.map(name => name.replace("Categoría:", "").trim()));
         arrayNamesGenious.push(...physic.map(name => name.replace("Categoría:", "").trim()));
         arrayNamesGenious.push(...informatic);
@@ -50,7 +53,37 @@ export class GalleryComponent  implements OnInit {
   }
 
   getPhotos(arrayNamesGenious: string[], arrayPhotosGenious: string[]){
-    // Aquí hacemos la petición de las fotos de cada genio y las almacenamos en un array
+    // 1º Creamos un array con todos los Observables obtenidos, para luego suscribirnos a ellos
+    // map nos devuelve un nuevo array
+    const arrayObservable = arrayNamesGenious.map(name => {
+      return this.apiService.getImage(name).pipe(
+        catchError(err => {
+          console.log("Error al obtener la foto del genio : " + name, err);
+          return of(null); // Si hay error, devolvemos null en lugar de interrumpir el flujo  
+        })
+      );
+    })
+
+    // 2º Recorremos dicho array y esperamos a que se resuelvan todas las solicitude HTTP
+    forkJoin(arrayObservable).subscribe(
+      {
+        // Cuando todas los Ovservable han sido resueltas, obtenemos un array con las URL de las fotos
+        next: (photos) => {
+          // 'photos' es un array con todas las respuestas (pueden ser URLs o null)
+          photos.forEach((photoUrl, index) => {
+            if (photoUrl !== null) {
+              arrayPhotosGenious.push(photoUrl);
+            } else {
+              console.log(arrayNamesGenious[index] + " no tiene foto en la base de datos");
+            }
+          });
+        },
+        error : (err ) => {
+          console.log("Hubo un error al hacer las solicitudes:", err);
+        }
+      }
+    )
+
   }
 
 }
