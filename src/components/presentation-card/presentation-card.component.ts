3938316f11124777dc659b8ede373b1ad710cd5d
samuel <saylsev699@g.educaand.es>
2025-05-27 12:15:47 +0200

    Scroll terminado

--- src/components/presentation-card/presentation-card.component.ts
+++ src/components/presentation-card/presentation-card.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { IonicModule, ModalController } from '@ionic/angular';
+import { Component, Input,  ViewChild, AfterViewInit, ElementRef,OnDestroy } from '@angular/core';
+import { IonicModule, ModalController  } from '@ionic/angular';
 import { Genius } from 'src/app/models/Genius';
-
 
 @Component({
   selector: 'app-presentation-card',
@@ -9,11 +8,62 @@ import { Genius } from 'src/app/models/Genius';
   styleUrls: ['./presentation-card.component.scss'],
   imports: [IonicModule],
 })
-export class PresentationCardComponent {
+export class PresentationCardComponent implements AfterViewInit, OnDestroy {
 
   @Input() genius!: Genius;  
 
+  @ViewChild('scrollArea', { static: false})
+  scrollArea!: ElementRef<HTMLElement>;
+
+  isScrolling = false;
+  scrollY = 0;
+  private scrollTimeout?: any;
+
   constructor(private modalCtrl: ModalController) {}
+
+  ngAfterViewInit(){
+    const scroll = this.scrollArea.nativeElement;
+    scroll.addEventListener('scroll',this.onScroll);
+  }
+
+  ngOnDestroy() {
+    const scroll = this.scrollArea?.nativeElement;
+    if(scroll) {
+      scroll.removeEventListener('scroll', this.onScroll);
+    }
+    clearTimeout(this.scrollTimeout);
+  }
+
+  private onScroll = (event: Event) => {
+    const target = event.target as HTMLElement;
+    const newY = target.scrollTop;
+
+    if (!this.isScrolling) {
+      this.isScrolling = true;
+      this.handleScrollStart();
+    }
+
+    this.scrollY = newY;
+    this.handleScroll({ scrollTop: newY });
+
+    clearTimeout(this.scrollTimeout);
+    this.scrollTimeout = setTimeout(() => {
+      this.isScrolling = false;
+      this.handleScrollEnd();
+    }, 100);
+  };
+
+  private handleScrollStart() {
+    console.log('Scroll iniciado');
+  }
+
+  private handleScroll(detail: { scrollTop: number }) {
+    console.log(`Scroll en ${detail.scrollTop}px`);
+  }
+
+  private handleScrollEnd() {
+    console.log('Scroll terminado');
+  }
 
   closeCard() {
     this.modalCtrl.dismiss();
